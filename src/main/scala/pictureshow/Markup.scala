d2ceb530a8eb3e47532accb8c261af1c596a774c
Nathan Hamblen <nathanh@meetup.com>
2010-07-05 17:27:56 -0400

    more nicer

--- src/main/scala/pictureshow/Markup.scala
+++ src/main/scala/pictureshow/Markup.scala
@@ -11,7 +11,7 @@ trait Markup { self: IO with Resolver with Config =>
   /** loads and processes all markdown from configured sections */
   def mkSlides = {
     ((new xml.NodeBuffer, 0) /: sections) ((a, s) => {
-      val files = new URL(loadPath, "%s/%s.md" format (s, s)) :: Nil
+      val files = slurp("%s/%s.md" format (s, s))
       val fileRes = (((new scala.xml.NodeBuffer, a._2)  /: files)((m, f) => {
         val mdRes = md(f, m._2)
         (m._1 &+ mdRes._1, mdRes._2)
@@ -19,9 +19,7 @@ trait Markup { self: IO with Resolver with Config =>
       (a._1 &+ fileRes._1, fileRes._2)
     })._1
   }
-  private def md(fname: URL, index: Int) = {
-    /** s/fromFile/fromPath in scala 2.8 */
-    val content = scala.io.Source.fromURL(fname, "utf8").getLines.mkString("\n")
+  private def md(content: String, index: Int) = {
     val slides = content.split("!SLIDE")
     ((new xml.NodeBuffer, index) /: slides)( (a, s) => {
       val lines = s.split("\n")
