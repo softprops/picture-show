ef322ccfc9fdf53554644a964ec34646ede3ec5e
Nathan Hamblen <nathanh@meetup.com>
2010-07-06 01:23:32 -0400

    getLines includes the newline

--- src/main/scala/pictureshow/IO.scala
+++ src/main/scala/pictureshow/IO.scala
@@ -4,7 +4,7 @@ trait IO { self: Resolver =>
   import java.net.URL
   def exists(path: String) = slurp(path).isDefined
   def slurp(path: String) = try {
-    Some(scala.io.Source.fromURL(new URL(loadPath, path)).getLines.mkString("\n"))
+    Some(scala.io.Source.fromURL(new URL(loadPath, path)).getLines.mkString(""))
   } catch {
     case e: java.io.FileNotFoundException => None
   }
