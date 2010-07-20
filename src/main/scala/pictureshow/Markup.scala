59ab6c1fe6a41a5bf2ce0618cbd6e70709e5b989
doug tangren <d.tangren@gmail.com>
2010-07-19 23:44:35 -0400

    start process of targeting parser for slides

--- src/main/scala/pictureshow/Markup.scala
+++ src/main/scala/pictureshow/Markup.scala
@@ -14,23 +14,29 @@ trait Markup { self: IO with Resolver with Config with Logging =>
       val files = slurp("%s/%s.md" format (s, s))
       if(files.isEmpty) log("no file(s) at path %s/%s.md" format(s, s))
       val fileRes = (((new scala.xml.NodeBuffer, a._2)  /: files)((m, f) => {
-        val mdRes = md(f, m._2)
+        val mdRes = parse(f, m._2)
         (m._1 &+ mdRes._1, mdRes._2)
       }))
       (a._1 &+ fileRes._1, fileRes._2)
     })._1
   }
-  private def md(content: String, index: Int) = {
+  private def parse(content: String, index: Int) = {
+    var Meta = """(?i)(HTML)""".r
     val slides = content.split("!SLIDE")
     if(slides.isEmpty) log("no slides within file %s at index %s" format(content, index))
-    ((new xml.NodeBuffer, index) /: slides)( (a, s) => {
-      val lines = s.split("\n")
+    ((new xml.NodeBuffer, index) /: slides.drop(1))( (a, s) => {
       (a._1 &+ (<div class="content" id={"slide-%s" format a._2}>
-       <div class="container">
-        { parse(s) }
-       </div>
+       <div class="container">{
+         s.split("\n")(0).trim match {
+            case Meta(meta) => {
+              <div>{ xml.PCData( s.split("\n").toList.drop(1).mkString("") ) }</div>
+            } 
+            case _ => parseMarkdown(s) 
+          }
+        }</div>
       </div>), a._2 + 1)
     })
   }
-  private def parse(content: String) = toXHTML(knockoff(content))
+  
+  private def parseMarkdown(content: String) = toXHTML(knockoff(content))
 }
