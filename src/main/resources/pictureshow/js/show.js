d73d9ee1b4645ad43fea1ac3760712aca3dd3606
doug tangren <d.tangren@gmail.com>
2010-07-19 22:08:35 -0400

    refactoring js

--- src/main/resources/pictureshow/js/show.js
+++ src/main/resources/pictureshow/js/show.js
@@ -1,109 +1,96 @@
- (function($) {
-    var util = (function() {
-      return {
-        dimensions: function() {
-          return {
-            width: $(window).width(),
-            height: $(window).height()
-          };
-        }
-      };
-    })();
+var show = (function(){
+  var move = function(e) {
+    var DIRECTIONS = {
+      37: -1,     // >
+      39: 1,      // <
+      32: 1,      // _
+      13: 1,      // return
+      27: 'home', // esc
+      left: -1,
+      right: 1
+    };
+
+    if (dir = DIRECTIONS[e.which || e]) {
+      if (dir == 'home') {
+        e.preventDefault();
+        e.stopPropagation();
+        location.href = '/';
+      } else {
+        $('#instructions').slideUp(100);
+        show.setIndex(show.index() + dir);
+      }
+    }
+  };
     
-    var slides = (function() {
-      return {
-        all: function() { 
-          return $('#slides .content'); 
-        },
-        index: function() { 
-          return Number(document.location.hash.split('#')[1]); 
-        },
-        setIndex: function(i) {
-          var newSlide = '#slide-' + i;
-           if ($(newSlide).size() < 1) { return false; }
-           document.location.hash = '#' + i;
-           adjustSlides();
-        }
-      };
-    })();
+  var clickMove = function(e) {
+    if (e.pageX < ($(window).width() / 2)) {
+      move('left');
+    } else {
+      move('right');
+    }
+  }
+     
+  var dimensions = function() {
+    return {
+      width: $(window).width(),
+      height: $(window).height()
+    };
+  };
   
-   var setSlideDimensions = function() {
-     var d = util.dimensions();
-     $('#slides').height(d.height).width(d.width);
-     slides.all().height(d.height).width(d.width);
-   };
+  var setSlideDimensions = function() {
+    var d = dimensions();
+    $('#slides').height(d.height).width(d.width);
+    show.slides().height(d.height).width(d.width);
+  };
 
-   var showCurrentSlide = function() {
-     try {
-     var dimensions = util.dimensions();
-     var index = slides.index();
-     var offset = (index || 0) * dimensions.width;
+  var showCurrentSlide = function() {
+    var d = dimensions();
+    var index = show.index();
+    var offset = (index || 0) * d.width;
+    $('#reel').animate({ marginLeft: '-' + offset + 'px' }, 200);
+  };
 
-     $('#reel').animate({ marginLeft: '-' + offset + 'px' }, 200);
-   } catch(e) { alert(e);}
-   };
+  var verticalAlign = function() {
+    var d = dimensions();
+    var margin = (dimensions.height - $(this).height()) / 2;
+    $(this).css({ paddingTop: margin + 'px' });
+  };
 
-   var verticalAlign = function() {
-     var dimensions = util.dimensions();
-     var margin = (dimensions.height - $(this).height()) / 2;
-     $(this).css({ paddingTop: margin + 'px' });
-   };
+  var adjustSlides = function() {
+    setSlideDimensions();
+    showCurrentSlide();
+  };
 
-   var adjustSlides = function() {
-     setSlideDimensions();
-     showCurrentSlide();
-   };
-
-   var move = function(e) {
-     
-     var DIRECTIONS = {
-       37: -1,     // >
-       39: 1,      // <
-       32: 1,      // _
-       13: 1,      // return
-       27: 'home', // esc
-       left: -1,
-       right: 1
-     };
-
-     if (dir = DIRECTIONS[e.which || e]) {
-       if (dir == 'home') {
-         e.preventDefault();
-         e.stopPropagation();
-         location.href = '/';
-       } else {
-         $('#instructions').slideUp(100);
-         slides.setIndex(slides.index() + dir);
-       }
-     }
-   }
-
-   function clickMove(e) {
-     if (e.pageX < ($(window).width() / 2)) {
-       move('left');
-     } else {
-       move('right');
-     }
-   }
-
-   function hideInstructions() {
-     $('#instructions').slideUp(200);
-   }
-
+  function hideInstructions() {
+    $('#instructions').slideUp(200);
+  }
+    
    $(window).bind('resize', function() { adjustSlides(); });
-   
-   $(document).bind('keydown', move);
-   
-   $(document).bind('click', clickMove);
-   
-   $(document).ready(function() {
-     
-     slides.setIndex(slides.index() || 0);
-     
-     if (document.location.search.indexOf('notes') == 1) {
-       $('.notes').show();
-     }
 
-     window.setTimeout(hideInstructions, 2000);
-    });
-   })(jQuery);
+   $(document).bind('keydown', move);
+
+   $(document).bind('click', clickMove);
+    
+   return {
+      slides: function() { 
+        return $('#slides .content'); 
+      },
+      index: function() { 
+        return Number(document.location.hash.split('#')[1]); 
+      },
+      setIndex: function(i) {
+        var newSlide = '#slide-' + i;
+        if ($(newSlide).size() < 1) { return false; }
+        document.location.hash = '#' + i;
+        adjustSlides();
+      },
+      go: function() {
+        this.setIndex(this.index() || 0);
+        window.setTimeout(hideInstructions, 2000);
+      } 
+    };
+  })();
+
+ $(document).ready(function() {
+    show.go();
+  });
