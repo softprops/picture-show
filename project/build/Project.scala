221615196e9dca06ddb793dac9eae69713b48788
doug tangren <d.tangren@gmail.com>
2011-01-25 01:26:13 -0500

    dep version bumps

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -2,14 +2,15 @@ import sbt._
 class Project(info: ProjectInfo) extends DefaultProject(info) {
   val t_repo = "t_repo" at "http://tristanhunt.com:8081/content/groups/public/"
   val knockoff = "com.tristanhunt" %% "knockoff" % "0.7.0-10"
-  
+
   val webroot = outputPath / "resources"
-  
-  val uf_vers = "0.2.2"
+
+  val uf_vers = "0.3.1"
   val uff = "net.databinder" %% "unfiltered-filter" % uf_vers
   val ufj = "net.databinder" %% "unfiltered-jetty" % uf_vers
-  
+
   // testing
-  val snapshots = "Scala Tools Snapshots" at "http://www.scala-tools.org/repo-snapshots/"
-  val specs = "org.scala-tools.testing" % "specs" % "1.6.2.2-SNAPSHOT" % "test"
+ // val snapshots = "Scala Tools Snapshots" at "http://www.scala-tools.org/repo-snapshots/"
+
+  val specs = "org.scala-tools.testing" % "specs" % "1.6.2.2" % "test"
 }
