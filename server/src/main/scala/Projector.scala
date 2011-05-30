2dc2da777220791688727d73f652b52c25f625e8
softprops <d.tangren@gmail.com>
2011-05-29 22:36:47 -0400

    a projector 'is' a renderer

--- server/src/main/scala/Projector.scala
+++ server/src/main/scala/Projector.scala
@@ -5,11 +5,9 @@ import unfiltered.request._
 import unfiltered.response._
 import java.net.URL
 
-class Projector(path: URL) extends Resolver(path)
-  with Config with IO with Markup with Templates
-  with Logging with unfiltered.filter.Plan {
+class Projector(path: URL) extends Renderer(path) with unfiltered.filter.Plan {
   def intent = {
     case Path("/") => HtmlContent ~>
-      ResponseString(render(css(combineCss), mkSlides, js(combineJs)).toString)
+      ResponseString(renderDefault)
   }
 }
