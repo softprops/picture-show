13d781574d95f0841993f1010e07a473f6cd1a62
softprops <d.tangren@gmail.com>
2011-05-31 00:13:53 -0400

    provide server or offline option

--- conscript/src/main/scala/conscripted.scala
+++ conscript/src/main/scala/conscripted.scala
@@ -1,15 +1,43 @@
 package pictureshow
 
 class PictureShowScript extends xsbti.AppMain {
+
+  /**
+   * --offline
+   * --server
+   *
+   */
+  object Options {
+    /** which mode to use */
+    val Server = "server"
+    val Offline = "offline"
+    val ServerOpt = """^--(\w)?(%s)""".format(Server).r
+    val OfflineOpt = """^--(\w)?(%s)""".format(Offline).r
+    def apply(args: Array[String]) =
+      (Server /: args)({ (mode, arg) =>
+        arg match {
+          case ServerOpt(_, s) => s
+          case OfflineOpt(_, o) => o
+          case _ => mode
+        }
+      })
+
+  }
+
   case class Exit(val code: Int) extends xsbti.Exit
 
   def run(config: xsbti.AppConfiguration) = {
-    Server.instance(config.arguments)(err => {
-      System.err.println(err)
-      Exit(1)
-    }).run({
-      _ => println("thank you for watching")
-    })
+    Options(config.arguments) match {
+      case Options.Server =>
+        Server.instance(config.arguments)(err => {
+          System.err.println(err)
+          Exit(1)
+        }).run({
+          _ => println("thank you for watching")
+        })
+      case Options.Offline =>
+         offline.Main.main(config.arguments)
+    }
     Exit(0)
   }
 }
