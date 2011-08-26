73aecb9b3659383993342a1b51ecf28c32378b38
Gerolf Seitz <gerolf.seitz@gmail.com>
2011-08-26 23:42:58 +0200

    Migrate to sbt-0.10.1

---
+++ project/build.scala
@@ -0,0 +1,71 @@
+import sbt._
+import Keys._
+
+object build extends Build {
+  
+  import Deps._
+
+  lazy val standardSettings = Defaults.defaultSettings ++ Seq(
+    organization := "me.lessis",
+    version := "0.1.0-SNAPSHOT",
+    scalaVersion := "2.7.7",
+    libraryDependencies += knockoff
+  )
+
+  lazy val root = Project(
+    id = "PictureShow",
+    base = file("."),
+    aggregate = Seq(core, server, offln, conscript)
+  )
+
+  /** core transformations from txt files to html slide formated html */
+  lazy val core = Project(
+    id = "PictureShow Core",
+    base = file("core"), 
+    settings = standardSettings ++ Seq(
+      libraryDependencies ++= Seq(codec, specs) 
+    )
+  )
+
+  /** serves generated html on a configurable port */
+  lazy val server = Project(
+    id = "PictureShow Server",
+    base = file("server"),  
+    settings = standardSettings ++ Seq(
+      libraryDependencies ++= Seq(uff, ufj)
+    ),
+    dependencies = Seq(core)  
+  )
+
+
+  /** caches generated html to disk */
+  lazy val offln = Project(
+    id = "PictureShow Offline",
+    base = file("offline"),
+    settings = standardSettings,
+    dependencies = Seq(core)
+  )
+
+  /** command line client */
+  lazy val conscript = Project(
+    id = "PictureShow Conscript", 
+    base = file("conscript"),
+    settings = standardSettings ++ Seq(
+      resolvers += Resolver.url("technically.us", new java.net.URL("http://databinder.net/repo/"))(Resolver.ivyStylePatterns),
+      libraryDependencies += launch
+    ),
+    dependencies = Seq(core, server, offln)
+  )
+
+
+
+  object Deps {
+    val knockoff = "net.databinder" %% "pamflet-knockoff" % "0.2.2"
+    val codec = "commons-codec" % "commons-codec" % "1.4"
+    val specs = "org.scala-tools.testing" % "specs" % "1.6.2.2" % "test"
+    val uf_version = "0.3.3"
+    val uff = "net.databinder" %% "unfiltered-filter" % uf_version
+    val ufj = "net.databinder" %% "unfiltered-jetty" % uf_version
+    val launch = "org.scala-tools.sbt" % "launcher-interface" % "0.7.4" % "provided"
+  }
+}
