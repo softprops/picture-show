2c9978c2acc1455548b1ab92732ae698b5a72c79
Nathan Hamblen <nathan@technically.us>
2011-10-18 00:02:14 -0400

    handle getlines api change in scala 2.8

--- core/src/main/scala/IO.scala
+++ core/src/main/scala/IO.scala
@@ -4,7 +4,7 @@ trait IO { self: Resolver =>
   import java.net.URL
   def exists(path: String) = slurp(path).isDefined
   def slurp(path: String) = try {
-    Some(scala.io.Source.fromURL(new URL(loadPath, path), "utf-8").getLines.mkString(""))
+    Some(scala.io.Source.fromURL(new URL(loadPath, path), "utf-8").mkString(""))
   } catch {
     case e: java.io.FileNotFoundException => None
   }
